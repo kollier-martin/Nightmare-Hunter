5f2a306fcc331301f109d5e5adca5762d38d8a28
kollier-martin <kollierm@yahoo.com>
2019-12-03 17:18:33 -0500

    Patch
    Scaling Issues Handled.
    
    Animator root motion sucks. Disabled it temporarily
    
    Level design updates
    
    Introduced Health Pots
    
    Changed jump height and player speed
    
    Monsters can damage and drop health pots
    
    Changed save and load method to JSON
    
    Marlo can now attack at an angle, but doesn't collide with the outside walls
    
    New music and music queues
    
    Fixed controller input handling and camera switching handling
    
    Fixed gun aiming and bullet instantiation

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sign : MonoBehaviour
+{
+    [SerializeField] private GameObject tooltip;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            tooltip.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tooltip.SetActive(false);
+        }
+    }
+}
